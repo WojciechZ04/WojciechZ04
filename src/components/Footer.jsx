18d5be1e636add26ccc72be1a44698de919cc540
Wojciech Zieliński <wojtekz.6776@gmail.com>
2023-06-30 20:23:09 +0200

    Update src: Add navbar, footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+	return(
+		<div>
+			<p>Made by Wojciech Zieliński</p>
+		</div>
+	)
+}
+
+export default Footer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+	return (
+		<header className="App-header">
+        	<h1>Hello there!</h1>
+    	</header>
+	)
+}
+
+export default Home;
