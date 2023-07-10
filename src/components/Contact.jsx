c776a7295fb0b55d785f8ecff1934d1bc102894b
Wojciech Zieliński <wojtekz.6776@gmail.com>
2023-07-10 20:21:37 +0200

    Update: add contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,45 @@
-import React from "react";
-import { Container } from "@mui/material";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-function Contact() {
-	return (
-		<Container id="contact" sx={{pt: "50px"}}>
-			<h2>Kontakt ze mną</h2>
-		</Container>
-	)
-}
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_347906d",
+        "template_7xb192n",
+        form.current,
+        "49uuStra43V6sGSH8"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="contact">
+	<h2>Kontakt ze mną</h2>
+	<p>Wyślij wiadomść za pomocą poniższego formularza</p>
+      <form className="form" ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
