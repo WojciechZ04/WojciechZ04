e659c744ae97e4b70f984611b4f2cd9a1ec11025
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-04-15 15:51:28 +0200

    hero changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <p>This webstie is still under construction</p>
+      <p>This website is still under construction</p>
     </div>
   );
 }
