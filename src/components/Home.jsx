64740ad156fecabecfcbf88819f796b47e32a790
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-04-18 23:00:43 +0200

    particles background

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Particles, {initParticlesEngine} from "@tsparticles/react";
+import {loadFull} from "tsparticles";
+import particlesOptions from "../assets/particles.json";
+
 
 function Home() {
+
+  const [init, setInit] = useState(false);
+
+    useEffect(() => {
+        if (init) {
+            return;
+        }
+
+        initParticlesEngine(async (engine) => {
+            await loadFull(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
   return (
     <div id="home">
+      {init && <Particles id="particles" options={particlesOptions}/>}
       <h1>Hi, I'm Wojciech Zieliński</h1>
       <div className="divider div-transparent div-arrow-down"></div>
       <p>A Frontend and Backend Junior Developer of Web Applications</p>
