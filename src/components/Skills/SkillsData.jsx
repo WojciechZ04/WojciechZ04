b8503647f1d3458d472b8e4e267350a6e79f2e0a
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-05-29 23:35:35 +0200

    add more skills to list

--- src/components/Skills/SkillsData.jsx
+++ src/components/Skills/SkillsData.jsx
@@ -1,6 +1,64 @@
 const skillsData = [
-	{ name: "HTML", icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/html-2752158-2284975.png?f=webp", level: 4 },
-	{ name: "CSS", icon: "https://static-00.iconduck.com/assets.00/file-type-css-icon-1806x2048-r5fwjl3p.png", level: 2 },
-]
+  {
+    name: "HTML",
+    icon: "https://cdn.iconscout.com/icon/premium/png-256-thumb/html-2752158-2284975.png?f=webp",
+    level: 4,
+  },
+  {
+    name: "CSS",
+    icon: "https://static-00.iconduck.com/assets.00/file-type-css-icon-1806x2048-r5fwjl3p.png",
+    level: 2,
+  },
+  {
+    name: "JavaScript",
+    icon: "",
+    level: 3,
+  },
+  {
+    name: "Node.js",
+    icon: "",
+    level: 2,
+  },
+  {
+    name: "React",
+    icon: "",
+    level: 3,
+  },
+  {
+    name: "SQL",
+    icon: "",
+    level: 2,
+  },
+  {
+    name: "VSC env",
+    icon: "",
+    level: 3,
+  },
+  {
+    name: "jQuery",
+    icon: "",
+    level: 1,
+  },
+  {
+    name: "EJS",
+    icon: "",
+    level: 1,
+  },
+  {
+    name: "GitHub",
+    icon: "",
+    level: 3,
+  },
+  {
+    name: "Angular",
+    icon: "",
+    level: 2,
+  },
+  {
+    name: "Typescript",
+    icon: "",
+    level: 2,
+  },
+];
 
 export default skillsData;
