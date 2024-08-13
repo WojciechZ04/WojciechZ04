610c76a7d85eea2c2635dd41f029956e4c51cd57
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-08-13 11:14:33 +0200

    add a photo of me, add background when particles are loading

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
+import Me from "../../images/me.jpg";
 import { Container, Grid, Button } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
@@ -31,27 +32,31 @@ function About() {
   function handleScroll(event, targetId) {
     event.preventDefault();
     const targetElement = document.getElementById(targetId);
-    targetElement.scrollIntoView({ behavior: 'smooth' });
+    targetElement.scrollIntoView({ behavior: "smooth" });
   }
 
   return (
     <Container id="about" className="section">
-      <CustomGridContainer container spacing={1} sx={{ pt: "50px" }}>
+      <CustomGridContainer
+        container
+        spacing={1}
+        sx={{ pt: "50px", alignItems: "center" }}
+      >
         <CustomGridItem item xs={12} sm={8}>
           <h2>Get to know me!</h2>
           <p>
-            I am a self-proclaimed programmer constantly improving my skills in
-            terms of programming and technology. I really enjoy extending my
-            knowledge in this categories. I'm learning from courses on Udemy and
-            by creating my own projects (more info about my projects below). If
-            you would like to know more or have any questions, feel free to
-            contact me!
+            I am a Frontend programmer constantly improving my skills in terms
+            of programming and technology. I really enjoy extending my knowledge
+            in this categories. I'm learning from courses on Udemy and by
+            creating my own projects (more info about my projects below). If you
+            would like to know more or have any questions, feel free to contact
+            me!
           </p>
           <ThemeProvider theme={theme}>
             <Button
               color="primary"
               variant="contained"
-              onClick={(event) => handleScroll(event, 'contact')}
+              onClick={(event) => handleScroll(event, "contact")}
               sx={{ margin: "0 auto", display: "block", width: "fit-content" }}
             >
               Contact
@@ -59,7 +64,17 @@ function About() {
           </ThemeProvider>
         </CustomGridItem>
         <CustomGridItem item xs={12} sm={4}>
-          <img src="https://img.freepik.com/premium-photo/portrait-successful-software-developer-eyeglasses-standing-with-arms-crossed-smiling-camera-office_249974-4145.jpg"/>
+          <img
+            src={Me}
+            alt="There should be me here, but it's not working"
+            style={{
+              display: "block",
+              margin: "0 auto",
+              width: "100%",
+              maxWidth: "400px",
+              height: "auto",
+            }}
+          />
         </CustomGridItem>
       </CustomGridContainer>
     </Container>
--- src/components/Hero/Home.jsx
+++ src/components/Hero/Home.jsx
@@ -1,29 +1,24 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./Hero.css";
-import Particles, {initParticlesEngine} from "@tsparticles/react";
-import {loadFull} from "tsparticles";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
 import particlesOptions from "../../assets/particles.json";
 
-
 function Home() {
+  const [loading, setLoading] = useState(true);
 
-  const [init, setInit] = useState(false);
-
-    useEffect(() => {
-        if (init) {
-            return;
-        }
-
-        initParticlesEngine(async (engine) => {
-            await loadFull(engine);
-        }).then(() => {
-            setInit(true);
-        });
-    }, [init]);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <div id="home">
-      {init && <Particles id="particles" options={particlesOptions}/>}
+      <div className={`loading-screen ${loading ? "" : "hidden"}`}></div>
+      <Particles id="particles" options={particlesOptions} />
       <h1>Hi, I'm Wojciech Zieliński</h1>
       <div className="divider div-transparent div-arrow-down white"></div>
       <p>A Frontend and Backend Junior Developer of Web Applications</p>
